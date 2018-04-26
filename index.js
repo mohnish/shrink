b45160e162f51234eb5ea3c275cd73dd10bc6493
Mohnish Thallavajhula <hi@iam.mt>
2018-04-26 14:00:05 -0700

    use image min for now

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const {execFile} = require('child_process');
-const guetzli = require('guetzli');
+#!/usr/bin/env node
+const program = require('commander');
+const imagemin = require('imagemin');
+const imageminGuetzli = require('imagemin-guetzli');
 
-module.exports = (img) => {
-  return execFile(guetzli, [img, 'output.jpg'], err => {
-    console.log('Image minified!');
-  });
-};
+program
+  .version('0.0.5')
+  .option('-f, --file <path>', 'Path to image file')
+  .parse(process.argv);
+
+imagemin(['*.{png,jpg}'], 'build/images', {
+  use: [
+      imageminGuetzli({quality: 95})
+  ]
+}).then(() => {
+    console.log('Images optimized');
+});
